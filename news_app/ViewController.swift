03ec099086575feb629db2e4a7549c0f81d14207
phattarapon <phattarapon.cpe61346@gmail.com>
2023-07-22 16:01:00 +0700

    Initial Commit

---
+++ news_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news_app
+//
+//  Created by Phattarapon Jungtakarn on 22/7/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
